d873fe6ba9dfc9b72e660b6b41985e57a5a94485
mraaj <mraajgi@gmail.com>
2022-03-11 21:27:52 +0000

    Modified Simpify01

--- simpy01.py
+++ simpy01.py
@@ -3,4 +3,11 @@ Fav_lang = "Kannada"
 print(f" Hi, i am {fn}, i am learning {Fav_lang}.")
 print("This is awesome")
 
+Gn = "Awsome"
+Bn = "Daddy"
+Print(f"I have {Gn} {BN})
+Print (50+50"% True")
+
+
+
 
